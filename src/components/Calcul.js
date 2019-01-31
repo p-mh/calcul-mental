72cdd4bdcad2f3685c1728c7de8b56e2e0d44d11
p-mh <pauline.miele@gmail.com>
2019-01-31 10:43:36 +0100

    good answers

---
+++ src/components/Calcul.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+const RESETSTATE = {
+  nbsToCalcul: [],
+  answer: null,
+  inputValue: '',
+};
+
+export default class Calcul extends Component {
+  constructor(props) {
+    super(props);
+    this.state = RESETSTATE;
+  }
+  componentDidMount() {
+    this.generateNewCalcul();
+  }
+
+  generateNewCalcul = () => {
+    if (!this.state.nbsToCalcul.length) {
+      const nb1 = Math.floor(Math.random() * 10);
+      const nb2 = Math.floor(Math.random() * 10);
+      this.setState({
+        nbsToCalcul: [nb1, nb2],
+        answer: nb1 + nb2,
+      });
+    }
+  };
+
+  onChange = ({ target: { value } }) => {
+    this.setState({
+      inputValue: value,
+    });
+  };
+
+  onKeyDown = e => {
+    if (e.key === 'Enter') {
+      this.try();
+    }
+  };
+
+  try = () => {
+    if (Number(this.state.inputValue) === this.state.answer) {
+      this.setState(RESETSTATE, this.generateNewCalcul);
+      this.props.addOnePoint();
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <p>
+          {this.state.nbsToCalcul[0]} + {this.state.nbsToCalcul[1]}
+        </p>
+        <input
+          type="text"
+          onChange={e => this.onChange(e)}
+          value={this.state.inputValue}
+          onKeyDown={e => this.onKeyDown(e)}
+        />
+        <button onClick={this.try}>Try</button>
+      </div>
+    );
+  }
+}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,59 +1,26 @@
 import React, { Component } from 'react';
+import Calcul from './Calcul';
 
-const RESETSTATE = {
-  nbsToCalcul: [],
-  answer: null,
-  inputValue: '',
-};
+const addPoint = (prevState, props) => ({
+  goodAnswers: Number(prevState.goodAnswers) + 1,
+});
 
 export default class Game extends Component {
-  state = RESETSTATE;
-  componentDidMount() {
-    this.generateNewCalcul();
-  }
-
-  generateNewCalcul = () => {
-    if (!this.state.nbsToCalcul.length) {
-      const nb1 = Math.floor(Math.random() * 10);
-      const nb2 = Math.floor(Math.random() * 10);
-      this.setState({
-        nbsToCalcul: [nb1, nb2],
-        answer: nb1 + nb2,
-      });
-    }
+  state = {
+    gameState: null,
+    goodAnswers: 0,
+    timer: 0,
   };
 
-  onChange = ({ target: { value } }) => {
-    this.setState({
-      inputValue: value,
-    });
-  };
-
-  onKeyDown = e => {
-    if (e.key === 'Enter') {
-      this.try();
-    }
-  };
-
-  try = () => {
-    if (Number(this.state.inputValue) === this.state.answer) {
-      this.setState(RESETSTATE, this.generateNewCalcul);
-    }
+  addOnePoint = () => {
+    this.setState(addPoint);
   };
 
   render() {
     return (
       <div>
-        <p>
-          {this.state.nbsToCalcul[0]} + {this.state.nbsToCalcul[1]}
-        </p>
-        <input
-          type="text"
-          onChange={e => this.onChange(e)}
-          value={this.state.inputValue}
-          onKeyDown={e => this.onKeyDown(e)}
-        />
-        <button onClick={this.try}>Try</button>
+        <Calcul addOnePoint={this.addOnePoint} />
+        <p>Good answers: {this.state.goodAnswers}</p>
       </div>
     );
   }
